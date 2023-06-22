5ebfe6759ebbc36c5f249fe15c029c51e8f7a4fc
TANUJ BISHT <mamtabisht123443@gmail.com>
2023-06-22 21:26:32 +0530

    Update revise.py
    small changes like added comment and added condition for 1 in prime and twisted prime.

--- revise.py
+++ revise.py
@@ -1,8 +1,10 @@
 
-#prime number checker
+# ====PRIME NUMBER====
 def prime(x):
     if x <= 0:
         print(x, ' should be natural number to be prime')
+    elif x == 1:
+        print('1 is not a prime number')
     else:
         f = 0
         for i in range(2, x):
@@ -13,17 +15,20 @@ def prime(x):
         else:
             print(x, ' is a prime number')
 
-#twister prime checker
+
+# ====TWISTED PRIME NUMBER====
 def twisted_prime(x):
     if x <= 0:
-        print(x, ' should be natural number to be twisted_prime')
+        print(x, ' should be natural number to be twisted prime number')
+    elif x == 1:
+        print('1 is not a twisted prime numbe')
     else:
         f = 0
         for i in range(2, x):
             if x % i == 0:
                 f = 1
         if f == 1:
-            print(x, ' is not a prime and twisted_prime number')
+            print(x, ' is not a prime and twisted prime number')
         else:
             str_x = ''
             for i in str(x):
@@ -33,25 +38,26 @@ def twisted_prime(x):
                 if int(str_x) % i == 0:
                     k = 1
             if k == 1:
-                print(x, ' is a prime number but not twisted_prime number')
+                print(x, ' is a prime number but not twisted prime number')
             else:
-                print(x, ' is  a twisted_prime number')
+                print(x, ' is  a twisted prime number')
 
-                
-#magic number checker
+
+# ====MAGIC NUMBER====
 def magic(x):
     sum = 0
     for i in str(x):
         sum += int(i)
     if sum < 10:
         if sum == 1:
-            print('magic number')
+            print(x, 'is a magic number')
         else:
-            print('not magic number')
+            print(x, 'is not a magic number')
     else:
         magic(sum)
 
-#amstrong number checker
+
+# ====AMSTRONG NUMBER====
 def amstrong(x):
     a = 0
     b = 0
@@ -59,13 +65,13 @@ def amstrong(x):
         a += 1
     for i in str(x):
         b += int(i)**a
-
     if b == x:
         print(x, ' is a amstrong number')
     else:
         print(x, ' is not a amstrong number')
 
-#perfect number checker
+
+# ====PERFECT NUMBER====
 def perfect(x):
     list = []
     for i in range(1, x):
@@ -79,7 +85,8 @@ def perfect(x):
     else:
         print(x, ' is not a prime number')
 
-#pallendrome numbers checker
+
+# ====PALLENDROME NUMBER====
 def pallendrome(x):
     k = str(x)
     str1 = ''
@@ -90,7 +97,8 @@ def pallendrome(x):
     else:
         print(x, 'is not a pallendrome number')
 
-#fibonacci series
+
+# ====FIBONACCI NUMBER====
 def fibonacci(x):
     a, b, c = 1, 1, 0
     while (c <= x):
@@ -98,8 +106,9 @@ def fibonacci(x):
         print(a, ' + ', b, ' = ', c)
         a = b
         b = c
-        
-#guess the number from 1 to 100 to win 
+
+
+# ====GUESS THE RANDOM NUMBER GAME====
 def random_number_guess_game():
     import random
     a = random.randint(1, 100)
@@ -120,35 +129,23 @@ def random_number_guess_game():
     else:
         print(f'NOICE !! you r good at this :) \n you take only {c} chances')
 
-#rock paper scissor game
-def rock_paper_scissor():
 
+# ====ROCK PAPER SCISSOR (NOT A SEPRATE UI)====
+def rock_paper_scissor():
     import random
     run = 1
     while run:
         a = int(input('_____________________\n|enter 0 for rock   |\n|                   |\n|enter 1 for paper  |\n|                   |\n|enter 2 for scissor|\n|___________________|\n :- '))
-        l = ['Rock','Paper','Scissor']
+        l = ['Rock', 'Paper', 'Scissor']
         b = random.choice(l)
         a = l[a]
-
-        if a == 'Rock' and b == 'scissor' :
+        if a == 'Rock' and b == 'scissor':
             print(f'win machine entered {b}')
-        elif a == 'Paper' and b == 'Rock' :
+        elif a == 'Paper' and b == 'Rock':
             print(f'win machine entered {b}')
-        elif a == 'Scissor' and b == 'Paper' :
+        elif a == 'Scissor' and b == 'Paper':
             print(f'Win machine entered {b}')
-        else: 
+        else:
             print(f'lose machine entered {b}')
 
-        run = int(input('enter 1 to continue and 0 to stop'))  
-        
-        
-#password generator 8 characters
-def password_gen8():
-    import random
-    charecters_list = [f'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S','T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','$','@','!','*','%','&'
-                   ]
-    password = ''
-    for i in range(8):
-        password += random.choice(charecters_list)
-    print(f'you password is :- {password}')
+        run = int(input('enter 1 to continue and 0 to stop'))
